d2bcad97a60e7cdd78f3bf63e5e7cc785b2a18c2
Lucia Ĺopez <lopezamayol@gmail.com>
2022-10-06 10:30:47 -0500

    capturando el nombre de usuario que ingrese a jugar

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 document.querySelector('.viewWelcome').classList.add('show');
 document.querySelector('.viewGame').classList.add('hidden');
+let getForm = document.getElementById('form-box');
+let getInputText = document.getElementById('inputName');
+
+getForm.addEventListener('submit', getNameUser);
+function getNameUser(e) {
+    e.preventDefault();
+    let nameUser = getInputText.value;
+    console.log(nameUser);
+}
